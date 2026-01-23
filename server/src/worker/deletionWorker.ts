75dc47d7acd7c971bdbacf3d86c849c92b35893d
devanshu0x <devanshudotdev@gmail.com>
2026-01-24 01:14:37 +0530

    fixed deletion worker

--- server/src/worker/deletionWorker.ts
+++ server/src/worker/deletionWorker.ts
@@ -4,28 +4,51 @@ import type { FileDeletionQueue } from "../config/bullmqConfig.js";
 import { vectorStore } from "../config/langchainConfig.js";
 import path from "path";
 import fs from "fs/promises"
+import { fileURLToPath } from "url";
 
 
-const worker= new Worker<FileDeletionQueue>("file-deletion-queue",
-    async (job)=>{
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const worker = new Worker<FileDeletionQueue>("file-deletion-queue",
+    async (job) => {
         console.log("New deletion job");
-        const {fileId,fileNameInDb}= job.data;
+        const { fileId, fileNameInDb } = job.data;
 
-        await vectorStore.delete({
-            filter:{
-                fileId
-            }
-        })
+        try {
+            await vectorStore.delete({
+                filter: {
+                    must: [
+                        {
+                            key: "metadata.fileId",
+                            match: {
+                                value: fileId,
+                            },
+                        },
+                    ],
+                },
+            });
+
+        } catch (err) {
+            console.error("Failed to delete from vectordb ", err);
+        }
 
         const UPLOAD_DIR = path.resolve(__dirname, "../../uploads");
         const filePath = path.join(UPLOAD_DIR, fileNameInDb);
 
-        await fs.unlink(filePath);
+        console.log(filePath);
+
+        try {
+            await fs.unlink(filePath);
+            console.log("File deleted:", filePath);
+        } catch (err: any) {
+            if (err.code !== "ENOENT") throw err;
+        }
     },
     {
-        connection:{
-            host:'localhost',
-            port:6379
+        connection: {
+            host: 'localhost',
+            port: 6379
         },
     }
 
